a72d87bae4abaaf5797120f28c06886ee4dbe8a8
Orlando <wiki97ap@gmail.com>
2021-01-19 13:42:21 -0600

    Filtro de respuestas por uid completado

--- lib/screens/Home_Screen.dart
+++ lib/screens/Home_Screen.dart
@@ -1,8 +1,7 @@
-// ignore: unused_import
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:pruebaVanglar/models/automodel.dart';
-// ignore: unused_import
+import 'package:pruebaVanglar/models/users.dart';
 import 'package:pruebaVanglar/widgets/Answer_Card.dart';
 
 class Home extends StatefulWidget {
@@ -14,53 +13,101 @@ class _HomeState extends State<Home> {
   Future fetchJsonData() async {
     var rawJson =
         await DefaultAssetBundle.of(context).loadString('Data/bd.json');
-
     Map mapValue = json.decode(rawJson)["answers"];
-
     List<Answer> itemList = List();
     mapValue.forEach((key, item) {
       itemList.add(new Answer.fromJson(item));
     });
-
-    /*itemList.forEach((item) {
-      print(item.answer);
-      print(item.userId);
-    });*/
+    // itemList.forEach((item) {
+    // print(item.answer);
+    // print(item.userId);
+    //});
     return itemList;
   }
 
+  List<Answer> allAnswers = List();
   List<Answer> myAnswers = List();
+  List<String> myUsers = UsersList.users;
 
+  bool loading = false;
+  String uidFilter;
   @override
   void initState() {
     getAnswers();
+
     super.initState();
   }
 
   getAnswers() {
     fetchJsonData().then((response) {
       setState(() {
-        myAnswers = response;
+        allAnswers = response;
+        myAnswers = customAnswer(
+          allAnswers,
+          'UserId.Q_UB_CB_EV4_BARZ84_PIV_UB_CZ5_GH_R_ZP2',
+        );
+
+        loading = true;
       });
     });
+  }
+
+  customAnswer(List<Answer> allAnswers, String uid) {
+    List<Answer> customList = List();
+    for (var i = 0; i < allAnswers.length; i++) {
+      if (allAnswers[i].userId.toString() == uid) {
+        customList.add(
+          new Answer(
+            answer: allAnswers[i].answer,
+            userId: allAnswers[i].userId,
+          ),
+        );
+      }
+    }
+    return customList;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(''),
+        actions: [
+          DropdownButton(
+            icon: Icon(Icons.search),
+            hint: Text(
+              'Filter by UID',
+              style: TextStyle(color: Colors.white),
+            ),
+            items: myUsers.map((e) {
+              return DropdownMenuItem(
+                child: Text('Alumno ${myUsers.indexOf(e)+1}'),
+                value: e,
+              );
+            }).toList(),
+            onChanged: (newValue) {
+              setState(() {
+                myAnswers = customAnswer(allAnswers, newValue);
+              });
+            },
+          ),
+        ],
+        title: Text(loading ? 'Answers' : 'Loading...'),
       ),
-      body: Center(
-        child: ListView.builder(
-          itemCount: myAnswers.length,
-          itemBuilder: (context, index) {
-            return AnswerCard(
-              answer: myAnswers[index].answer.toString(),
-            );
-          },
-        ),
-      ),
+      body: loading
+          ? Center(
+              child: ListView.builder(
+                itemCount: myAnswers.length,
+                itemBuilder: (context, index) {
+                  return AnswerCard(
+                    answer: myAnswers[index].answer.toString(),
+                    uid: myAnswers[index].userId.toString(),
+                  );
+                },
+              ),
+            )
+          : Center(
+              child: CircularProgressIndicator(),
+            ),
     );
   }
 }
--- lib/widgets/Answer_Card.dart
+++ lib/widgets/Answer_Card.dart
@@ -2,11 +2,35 @@ import 'package:flutter/material.dart';
 
 class AnswerCard extends StatelessWidget {
   final String answer;
-  AnswerCard({@required this.answer});
+  final String uid;
+  AnswerCard({@required this.answer, @required this.uid});
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Text(answer),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 2),
+      child: Card(
+        color: Colors.blue,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 5),
+          child: Column(
+            children: [
+              Text(
+                'Answer: $answer',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+                textAlign: TextAlign.start,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                '$uid',
+                textAlign: TextAlign.end,
+                style: TextStyle(fontSize: 10, color: Colors.white),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
